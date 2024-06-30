bd5ee3dda70f350e49b3cb6dcf4545fe1b47dcb4
irshadpp <irshad89434@gmail.com>
2024-06-30 16:06:30 +0530

    array types

---
+++ arrays.ts
@@ -0,0 +1,21 @@
+const strArr: string[] = [];
+strArr.push('fdsfa');
+
+
+// const numArr: number[] = [];
+const numArr: Array<Number> = [];
+numArr.push(897);
+
+
+type User = {
+    name:string
+    isActive: boolean
+}
+const allUsers: User[] = [];
+allUsers.push({name:'dfsaf', isActive:false});
+
+
+const twoDarr: number[][] = [
+    [323, 324, 425],
+    []
+]
